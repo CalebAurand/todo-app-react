92779f52241ec45b4b91e591ed456bc7472626b2
CalebAurand <caurand13@gmail.com>
2022-06-30 16:53:44 -0500

    finished basic functionality of rendering todo items with <ol>

--- src/App.js
+++ src/App.js
@@ -7,20 +7,44 @@ class App extends Component{
     super()
 
     this.state = {
-      isClicked: false,
+      isClicked: true,
+      inputValue: "",
+      listOfTodos: []
     }
   }
+
+  handleClick = () => {
+    this.state.isClicked ? 
+      this.setState({isClicked : false}) :
+        this.setState({isClicked : true})
+  }
+
+  handleChange = (event) => {
+    this.setState({inputValue: event.target.value})
+  }
+  
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({listOfTodos: [...this.state.listOfTodos, this.state.inputValue]})
+    this.setState({inputValue: ""})
+  }
+
+  mapTodos = (event) => {
+
+  }
+
   render(){
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            This is Caleb. I finally figured it out.
-          </p>
-          <p>
-            Well somewhat... figured it out. Yes!!!
-          </p>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" value={this.state.inputValue} onChange={this.handleChange}></input>
+            <button type="submit">Save/Submit</button>
+          </form>          
+          <ol>{this.state.listOfTodos.map((todoItem, index) => {
+            return <span><button>Done</button><li key={index}>{todoItem}</li></span>
+          })}</ol>
           <a
             className="App-link"
             href="https://reactjs.org"
